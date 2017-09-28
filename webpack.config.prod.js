f7682b435af5edbe4ffdddc937e4177d2dc9844a
offaxis <clement@offaxis.io>
2017-09-28 17:58:30 +0200

    Update publicPath

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -23,7 +23,7 @@ module.exports = {
   output: {
     path: __dirname + '/dist/client/',
     filename: '[name].[chunkhash].js',
-    publicPath: '/chat/',
+    publicPath: '/chat/', 
   },
 
   resolve: {

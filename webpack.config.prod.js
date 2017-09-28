d04ba13929bd437216676e6ca0c917db3e70c48d
offaxis <clement@offaxis.io>
2017-09-28 17:39:52 +0200

    Deploy prod on /chat/

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -23,7 +23,7 @@ module.exports = {
   output: {
     path: __dirname + '/dist/client/',
     filename: '[name].[chunkhash].js',
-    publicPath: '/',
+    publicPath: '/chat/',
   },
 
   resolve: {

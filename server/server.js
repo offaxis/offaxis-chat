e03fe56acbc4cf695656c66bed472e720cafcc76
offaxis <clement@offaxis.io>
2017-09-29 10:05:35 +0200

    Change Favicon

--- server/server.js
+++ server/server.js
@@ -98,7 +98,7 @@ const renderFullPage = (html, initialState) => {
 
         ${process.env.NODE_ENV === 'production' ? `<link rel='stylesheet' href='${assetsManifest['/app.css']}' />` : ''}
         <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'/>
-        <link rel="shortcut icon" href="http://res.cloudinary.com/hashnode/image/upload/v1455629445/static_imgs/mern/mern-favicon-circle-fill.png" type="image/png" />
+        <link rel="shortcut icon" href="http://offaxis.io/favicon.io" type="image/png" />
       </head>
       <body>
         <div id="root"><div>${html}</div></div>

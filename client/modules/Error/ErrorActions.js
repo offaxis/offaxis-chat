1cae1a017d3103382587e94b2dd9aa365f9cd93a
offaxis <clement@offaxis.io>
2017-10-05 12:08:32 +0200

    Error : Add error display management

--- client/modules/Error/ErrorActions.js
+++ client/modules/Error/ErrorActions.js
@@ -1,9 +1,26 @@
 
-export function displayErrors(response) {
-    if(response.errors) {
-        for(var i in response.errors) {
-            const error = response.errors[i];
-            alert(error.type + ' : ' + error.message);
-        }
+import Alert from 'react-s-alert';
+
+export function displayErrors(type, message) {
+    switch(type) {
+        case 'error':
+            Alert.error(message);
+            break;
+
+        case 'warning':
+            Alert.warning(message);
+            break;
+
+        case 'info':
+            Alert.info(message);
+            break;
+
+        case 'success':
+            Alert.success(message);
+            break;
+
+
+        default:
+            alert(type + ' : ' + message);
     }
 }
